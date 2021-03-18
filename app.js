125c301a855413cfd880900846b881370a1f147c
Anahi S <is715773@iteso.mx>
2021-03-18 17:53:30 -0600

    added POST to tone analyser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+
+require('dotenv').config();
 
 const router = require('./src/routes')
 const path = require('path');
@@ -13,22 +14,19 @@ const port = process.env.PORT || 4500
 
 const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 
-const toneAnalyzer = new ToneAnalyzerV3({
-    version: '2017-09-21',
-    iam_apikey: process.env.API_KEY,
-    url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
-});
 
 app.use(json())
 app.use(urlencoded({ extended: false }))
 
 
 app.use(cors(corsOptions))
-
 app.use(router)
+
 
 app.use('/home',(req,res) => { res.sendFile(path.join(__dirname+'/src/html/index.html')); })
 app.use('/', (req, res) => { res.send(`I don't Love Docker & Kubernetes & NodeJs`);})
+
+
 
 app.listen(port,host, () => { console.log(`Server listening on port ${port} in the host ${host}`); })
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 const autorRoute = require('./autor');
 
 router.use('/autor', autorRoute);
 
+const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({
+      apikey: process.env.API_KEY,
+    }),
+    serviceUrl: process.env.URL,
+    disableSslVerification: true,
+  });
+  
+router.post('/',async (req,res)=>{
+
+    console.log(req.body);
+    const toneParams = {
+        toneInput: { 'text': req.body.text },
+        content_type: 'application/json',
+        };
+
+        toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+        res.send(JSON.stringify(toneAnalysis, null, 2));
+        })
+        .catch(err => {
+        res.send('error:', err);
+        });
+});
 module.exports = router;
